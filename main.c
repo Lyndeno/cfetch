24d60ba068e88b8fbed70cf894fac930b34d7246
Lyndon Sanche <lsanche@lyndeno.ca>
2021-06-17 19:51:17 -0600

    Trying out uname in C

--- main.c
+++ main.c
@@ -1,5 +1,11 @@
 #include <stdio.h>
+#include <sys/utsname.h>
 
 int main(void) {
-	printf("Hello world!\n");
+	struct utsname local_machine;
+	uname(&local_machine);
+	printf(local_machine.sysname);
+//printf(local_machine->nodename);
+	printf(local_machine.release);
+//	printf(local_machine.version);
 }
