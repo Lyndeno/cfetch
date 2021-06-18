982531d62cb8d3cae19b1d250aa47920a1b7e28d
Lyndon Sanche <lsanche@lyndeno.ca>
2021-06-17 21:18:06 -0600

    Add some more system stuff

--- main.c
+++ main.c
@@ -1,15 +1,25 @@
 #include <stdio.h>
 #include <sys/utsname.h>
+#include <sys/sysinfo.h>
 #include <unistd.h>
+#include <math.h>
 
 #define HOST_NAME_MAX 64 // TODO: Figure out how to get this from the system
 
 int main(void) {
 	struct utsname local_machine;
 	uname(&local_machine);
-	printf("%s %s\n", local_machine.sysname, local_machine.release);
+	printf("Kernel: %s %s %s\n", local_machine.sysname, local_machine.release, local_machine.machine);
 
 	char hostname[HOST_NAME_MAX+1];
 	gethostname(hostname, HOST_NAME_MAX + 1);
-	printf(hostname);
+	printf("Host: %s\n",hostname);
+
+	struct sysinfo machine_info;
+	sysinfo(&machine_info);
+	printf("Uptime: %ld\n", machine_info.uptime);
+	// TODO: Memory reported from sysinfo is inaccurate, consider parsing /proc/meminfo
+	//printf("Buffer ram: %f\n", (machine_info.bufferram) / powf(1024,3));
+	//printf("mem unit: %u", machine_info.mem_unit);
+	//printf("RAM: %f / %f\n", (float)(machine_info.totalram - machine_info.freeram - machine_info.bufferram) / powf(1024,3), (float)machine_info.totalram / powf(1024,3));
 }
