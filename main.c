b218099d9505b21a08e5458021f43781633684ff
Lyndon Sanche <lsanche@lyndeno.ca>
2021-06-17 20:22:20 -0600

    Get hostname working

--- main.c
+++ main.c
@@ -1,11 +1,15 @@
 #include <stdio.h>
 #include <sys/utsname.h>
+#include <unistd.h>
+
+#define HOST_NAME_MAX 64 // TODO: Figure out how to get this from the system
 
 int main(void) {
 	struct utsname local_machine;
 	uname(&local_machine);
-	printf(local_machine.sysname);
-//printf(local_machine->nodename);
-	printf(local_machine.release);
-//	printf(local_machine.version);
+	printf("%s %s\n", local_machine.sysname, local_machine.release);
+
+	char hostname[HOST_NAME_MAX+1];
+	gethostname(hostname, HOST_NAME_MAX + 1);
+	printf(hostname);
 }
