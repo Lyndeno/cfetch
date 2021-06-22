ee9db45f5d63c87d99fbf97a8d477911c935aed6
Lyndon Sanche <lsanche@lyndeno.ca>
2021-06-22 14:22:22 -0600

    Format the uptime to show in either seconds/minutes/hours

--- main.c
+++ main.c
@@ -1,10 +1,13 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <sys/utsname.h>
 #include <sys/sysinfo.h>
 #include <unistd.h>
 #include <math.h>
 
 #define HOST_NAME_MAX 64 // TODO: Figure out how to get this from the system
+
+char *format_time(long);
 
 int main(void) {
 	struct utsname local_machine;
@@ -17,9 +20,26 @@ int main(void) {
 
 	struct sysinfo machine_info;
 	sysinfo(&machine_info);
-	printf(" Uptime: %ld\n", machine_info.uptime);
+	//printf(" Uptime: %ld\n", machine_info.uptime);
+	printf(" Uptime: %s\n", format_time(machine_info.uptime));
 	// TODO: Memory reported from sysinfo is inaccurate, consider parsing /proc/meminfo
 	//printf("Buffer ram: %f\n", (machine_info.bufferram) / powf(1024,3));
 	//printf("mem unit: %u", machine_info.mem_unit);
 	//printf("RAM: %f / %f\n", (float)(machine_info.totalram - machine_info.freeram - machine_info.bufferram) / powf(1024,3), (float)machine_info.totalram / powf(1024,3));
+}
+
+char *format_time(long uptime_seconds) {
+	char *buffer;
+	buffer = (char *)malloc(50*sizeof(char));
+	if (uptime_seconds < 60) {
+		sprintf(buffer, "%ld seconds", uptime_seconds);
+	} else if (uptime_seconds >= 60 && uptime_seconds < 3600) {
+		float uptime_minutes = (float)uptime_seconds / 60;
+		sprintf(buffer, "%f minutes", uptime_minutes);
+	}
+	else {
+		float uptime_hours = (float)uptime_seconds / 3600;
+		sprintf(buffer, "%f hours", uptime_hours);
+	}
+	return buffer;
 }
