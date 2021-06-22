5ea657afb75c92a6e44527cc7c62770ee77c63b0
Lyndon Sanche <lsanche@lyndeno.ca>
2021-06-22 14:03:11 -0600

    Add icons and align output

--- main.c
+++ main.c
@@ -9,15 +9,15 @@
 int main(void) {
 	struct utsname local_machine;
 	uname(&local_machine);
-	printf("Kernel: %s %s %s\n", local_machine.sysname, local_machine.release, local_machine.machine);
+	printf(" Kernel: %s %s %s\n", local_machine.sysname, local_machine.release, local_machine.machine);
 
 	char hostname[HOST_NAME_MAX+1];
 	gethostname(hostname, HOST_NAME_MAX + 1);
-	printf("Host: %s\n",hostname);
+	printf("   Host: %s\n",hostname);
 
 	struct sysinfo machine_info;
 	sysinfo(&machine_info);
-	printf("Uptime: %ld\n", machine_info.uptime);
+	printf(" Uptime: %ld\n", machine_info.uptime);
 	// TODO: Memory reported from sysinfo is inaccurate, consider parsing /proc/meminfo
 	//printf("Buffer ram: %f\n", (machine_info.bufferram) / powf(1024,3));
 	//printf("mem unit: %u", machine_info.mem_unit);
