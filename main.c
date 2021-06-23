cd7e718d9b219837147876544122bfaa1bb8ec91
Lyndon Sanche <lsanche@lyndeno.ca>
2021-06-22 19:11:18 -0600

    Remove buffer allocation from formatting function to avoid confusion

--- main.c
+++ main.c
@@ -7,7 +7,7 @@
 
 #define HOST_NAME_MAX 64 // TODO: Figure out how to get this from the system
 
-char *format_time(long);
+void format_time(char *, long);
 
 int main(void) {
 	struct utsname local_machine;
@@ -21,14 +21,16 @@ int main(void) {
 	struct sysinfo machine_info;
 	sysinfo(&machine_info);
 	//printf(" Uptime: %ld\n", machine_info.uptime);
-	printf(" Uptime: %s\n", format_time(machine_info.uptime));
+	char *uptime_buffer;
+	uptime_buffer = (char *)malloc(50*sizeof(char));
+	format_time(uptime_buffer, machine_info.uptime);
+	printf(" Uptime: %s\n", uptime_buffer); 
+	free(uptime_buffer);
 	// TODO: Memory reported from sysinfo is inaccurate, consider parsing /proc/meminfo
 }
 
-char *format_time(long uptime_seconds) {
+void format_time(char *buffer, long uptime_seconds) {
 	// TODO: Move buffer allocation out of this function, we don't want memory leaks later
-	char *buffer;
-	buffer = (char *)malloc(50*sizeof(char));
 	if (uptime_seconds < 60) {
 		sprintf(buffer, "%ld seconds", uptime_seconds);
 	} else if (uptime_seconds >= 60 && uptime_seconds < 3600) {
@@ -42,5 +44,4 @@ char *format_time(long uptime_seconds) {
 		long uptime_remaining_seconds = uptime_seconds - uptime_hours*3600 - uptime_minutes*60; 
 		sprintf(buffer, "%ld hours, %ld minutes, %ld seconds", uptime_hours, uptime_minutes, uptime_remaining_seconds);
 	}
-	return buffer;
 }
