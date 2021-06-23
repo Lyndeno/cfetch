98fbceab1f005a7d9bbf502ec15c39feb421324d
Lyndon Sanche <lsanche@lyndeno.ca>
2021-06-22 19:52:55 -0600

    Implement first parts of fetchline lists

--- main.c
+++ main.c
@@ -5,17 +5,29 @@
 #include <unistd.h>
 #include <math.h>
 
-#define HOST_NAME_MAX 64 // TODO: Figure out how to get this from the system
+#define SEPARATOR ": "
+#define CONTENT_MAX 64 // TODO: Implement dynamic string allocation for muh minimal resource usage
+#define TITLE_MAX 32
 
 void format_time(char *, long);
+
+typedef struct fetchline {
+	char *title;
+	char *content;
+	struct fetchline *next;
+} fetchline;
+
+fetchline *init_fetchline(void);
+void free_fetchline(fetchline *);
+void append_fetchline(fetchline *, fetchline *);
 
 int main(void) {
 	struct utsname local_machine;
 	uname(&local_machine);
 	printf(" Kernel: %s %s %s\n", local_machine.sysname, local_machine.release, local_machine.machine);
 
-	char hostname[HOST_NAME_MAX+1];
-	gethostname(hostname, HOST_NAME_MAX + 1);
+	char hostname[CONTENT_MAX+1];
+	gethostname(hostname, CONTENT_MAX+ 1);
 	printf("   Host: %s\n",hostname);
 
 	struct sysinfo machine_info;
@@ -44,4 +56,26 @@ void format_time(char *buffer, long uptime_seconds) {
 		long uptime_remaining_seconds = uptime_seconds - uptime_hours*3600 - uptime_minutes*60; 
 		sprintf(buffer, "%ld hours, %ld minutes, %ld seconds", uptime_hours, uptime_minutes, uptime_remaining_seconds);
 	}
+}
+
+fetchline *init_fetchline(void) {
+	fetchline *new_fetchline = (fetchline *)malloc(sizeof(fetchline));
+	new_fetchline->content = (char *)malloc(CONTENT_MAX*sizeof(char));
+	new_fetchline->title = (char *)malloc(TITLE_MAX*sizeof(char));
+	new_fetchline->next = NULL;
+
+	return new_fetchline;
+}
+
+void free_fetchline(fetchline *garbage) {
+	free(garbage->content);
+	free(garbage->title);
+	free(garbage);
+}
+
+void append_fetchline(fetchline *list_element, fetchline *new_fetchline) {
+	while (list_element->next != NULL) {
+		list_element = list_element->next;
+	}
+	list_element->next = new_fetchline;
 }
